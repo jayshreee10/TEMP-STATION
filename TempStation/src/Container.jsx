7fa5afd6574fe386130c9ee5f732413b126fd4c6
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-04-11 11:28:35 +0530

    finalizing  Ui Skeleton

--- TempStation/src/Container.jsx
+++ TempStation/src/Container.jsx
@@ -29,7 +29,7 @@ function TempStation() {
         <div
           className="FirstDataContainer"
           style={{
-            height: "30vh",
+            height: "25vh",
             width: "85vw",
             position: "absolute",
             top: "11vh",
@@ -43,8 +43,8 @@ function TempStation() {
           <div
             className="Card1"
             style={{
-              width: "30vw",
-              height: "25vh",
+              width: "25vw",
+              height: "20vh",
               backgroundColor: "pink",
               position: "absolute",
               left: "0vw",
@@ -58,28 +58,35 @@ function TempStation() {
             <div
               className="loaction"
               style={{
-                width: "20vw",
+                width: "15vw",
                 height: "10vh",
                 backgroundColor: "blue",
                 fontSize: "46px",
                 fontWeight: "400",
+                display:"flex",
+                alignItems:"center",
+                justifyContent:"center"
               }}
             >
               {" "}
-              Odisha{" "}
+              Odisha{""}
             </div>
             <div
               className="locationData"
               style={{
                 display: "flex",
-                width: "20vw",
-                height: "10vh",
+                width: "15vw",
+                height: "7vh",
                 backgroundColor: "red",
+                display:"flex",
+                alignItems:"center",
+                justifyContent:"space-evenly",
+
               }}
             >
-              <div className="day">Today | </div>
-              <div className="date"> 4/5/6 | </div>
-              <div className="time"> 3.56AM</div>
+              <div className="day"><b>Today</b> | </div>
+              <div className="date"> <b>4/5/6</b> | </div>
+              <div className="time"><b>3.56AM</b> </div>
             </div>
           </div>
 
@@ -107,7 +114,7 @@ function TempStation() {
                 width: "300px",
                 height: "36px",
                 backgroundColor: "white",
-                opacity:"60%",
+                opacity: "60%",
                 color: "black",
                 borderRadius: "6px",
               }}
@@ -135,48 +142,194 @@ function TempStation() {
         {/* data container box two  : card 3 = temp + celcius + weather condition (cloudy/sunny) ; card 4 = weather gif ; card 5 = extra data (feels like + min temp + max temp  )  */}
         <div
           className="SecondDataContainer"
-          style={{ display: "flex", height: "45vh", width: "85vw",backgroundColor:"yellow" }}
+          style={{
+            display: "flex",
+            height: "50vh",
+            width: "85vw",
+            backgroundColor: "yellow",
+          }}
         >
           <div className="Card3">
             {/* card 3.1 = temp */}
-            <div className="Temperature"> 40 </div>
+            <div
+              className="Temperature"
+              style={{
+                width: "20vw",
+                height: "15vh",
+                position: "absolute",
+                left: "10vw",
+                top: "37vh",
+                backgroundColor: "black",
+                color: "white",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                fontSize: "80px",
+              }}
+            >
+              {" "}
+              40 C{" "}
+            </div>
 
-            {/* card 3.2 = Unit in celcius*/}
-            <div className="Unit"> C|F</div>
+            {/* card 3.2 = Unit in celcius
+            <div className="Unit"> C|F</div> */}
 
-            {/* card 3.1 = weather condition (cloudy/sunny) */}
-            <div className="WeatherCondition">cloudy</div>
+            {/* card 3.2 = weather condition (cloudy/sunny) */}
+            <div
+              className="WeatherCondition"
+              style={{
+                width: "20vw",
+                height: "5vh",
+                position: "absolute",
+                left: "10vw",
+                top: "52vh",
+                backgroundColor: "blue",
+                color: "white",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                fontSize: "20px",
+              }}
+            >
+              cloudy
+            </div>
           </div>
 
           {/* card 4 = weather gif */}
-          <div className="Card4">gif url</div>
+          <div className="Card4" style={{
+            display: "flex",
+            flexDirection:"column",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+            right: "18.5vw",
+            top: "36vh",
+            height: "25vh",
+            width: "25vw",
+            backgroundColor: "brown",
+          }}>gif url</div>
 
           {/* card 5 = extra data (feels like + min temp + max temp  )   */}
-          <div className="Card5">
-            <div className="FeelsLike">feels like</div>
-            <div className="MinTemp">min temp</div>
-            <div className="MaxTemp">max temp</div>
+          <div className="Card5"  style={{
+            display: "flex",
+            flexDirection:"column",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+            right: "7.5vw",
+            top: "40vh",
+            height: "16vh",
+            width: "11vw",
+            backgroundColor: "red",
+          }}>
+            <div className="FeelsLike"><b>feels like</b></div>
+            <div className="MinTemp"><b>min temp</b></div>
+            <div className="MaxTemp"><b>max temp</b></div>
           </div>
         </div>
 
         {/* data container box three : card 6,7,8,9,10= extra data (  6: (sunrise + sunset) + 7: pressure + 8: humidity + 9: wind + 10: (sea_level + grnd_level) ) */}
-        <div className="ThirdDataContainer">
-          <div className="Card6">
+        <div
+          className="ThirdDataContainer"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+            height: "25vh",
+            width: "85vw",
+            backgroundColor: "red",
+          }}
+        >
+          <div
+            className="Card6"
+            style={{
+              height: "22vh",
+              width: "15vw",
+              backgroundColor: "black",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontSize: "15px",
+              flexDirection: "column",
+            }}
+          >
             {/* Card 6: (sunrise + sunset) */}
-            <div className="SunShine"></div>
+            <div className="SunShine">image</div>
+            <div className="SunShine">sunrise</div>
+            <div className="SunShine">sunset</div>
           </div>
 
           {/* Card 7: Pressure */}
-          <div className="Card7"> pressure </div>
+          <div
+            className="Card7"
+            style={{
+              height: "22vh",
+              width: "15vw",
+              backgroundColor: "black",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontSize: "15px",
+              flexDirection: "column",
+            }}
+          >
+             <div className="SunShine">image</div>
+            <div className="SunShine">pressure</div>
+          </div>
 
           {/* Card 8: Humidity */}
-          <div className="Card8"></div>
+          <div
+            className="Card8"
+            style={{
+              height: "22vh",
+              width: "15vw",
+              backgroundColor: "black",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontSize: "15px",
+              flexDirection: "column",
+            }}
+          >
+             <div className="SunShine">image</div>
+            <div className="SunShine">Humidity</div>
+          </div>
 
           {/* Card 9: wind */}
-          <div className="Card9"></div>
+          <div
+            className="Card9"
+            style={{
+              height: "22vh",
+              width: "15vw",
+              backgroundColor: "black",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontSize: "15px",
+              flexDirection: "column",
+            }}
+          >
+             <div className="SunShine">image</div>
+            <div className="SunShine">wind</div>
+          </div>
 
           {/* Card 10: sea_level + grnd_level  */}
-          <div className="Card10"></div>
+          <div
+            className="Card10"
+            style={{
+              height: "22vh",
+              width: "15vw",
+              backgroundColor: "black",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontSize: "15px",
+              flexDirection: "column",
+            }}
+          >
+             <div className="SunShine">image</div>
+            <div className="SunShine">sea_level</div>
+          </div>
         </div>
       </div>
     </div>
