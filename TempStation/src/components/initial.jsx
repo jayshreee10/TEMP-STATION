eeeca4a9f66b23c4bcdffb5157e75a2439226b54
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-04-14 20:38:52 +0530

    add : error feature

---
+++ TempStation/src/components/initial.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Lottie from 'lottie-react'
+import initialLottie from "../assets/lottie/initial.json"
+
+function initial() {
+  return (
+    <div style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center" , }}>
+    <Lottie animationData={initialLottie} style={{height:"50vh",width:"100vw"}}></Lottie>
+    <p style={{marginTop:"2%",marginLeft:"5%",fontWeight:"bolder",fontSize:"35px"}}> </p>
+    </div>
+  )
+}
+
+export default initial
--- TempStation/src/components/loading.jsx
+++ TempStation/src/components/loading.jsx
@@ -4,7 +4,7 @@ import loadingLottie from "../assets/lottie/loading.json"
 
 function loading() {
   return (
-    <div style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center",backgroundColor:"black"}}>
+    <div style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
     <Lottie animationData={loadingLottie} style={{height:"50vh",width:"100vw"}}></Lottie>
     {/* <p style={{marginTop:"2%",marginLeft:"5%",fontWeight:"bolder",fontSize:"35px"}}> </p> */}
 
