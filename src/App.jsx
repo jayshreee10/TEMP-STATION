4713ec86c7e697bcca8cf1d98e6e36272cd0c237
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-04-19 19:46:49 +0530

    update : meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
 import TempStation from "./Container";
-// import Error from "./components/error";
-// import Loading from "./components/loading";
-// import InitialLottie from "./components/initial";
+
+
+
 function App() {
 
   return (
     <TempStation/>
-    // <Error/>
-    // <Loading/>
-    // <InitialLottie/>
   )
    
 
