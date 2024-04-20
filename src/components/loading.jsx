afbda4f8f4d688a9d2d8edc76493f8435650e4f8
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-04-20 14:13:45 +0530

    update : loading component

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -6,7 +6,7 @@ function loading() {
   return (
     <div style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
     <Lottie animationData={loadingLottie} style={{height:"50vh",width:"100vw"}}></Lottie>
-    {/* <p style={{marginTop:"2%",marginLeft:"5%",fontWeight:"bolder",fontSize:"35px"}}> </p> */}
+    <p style={{marginTop:"2%",marginLeft:"5%",fontSize:"20px"}}> Searching.... </p>
 
     </div>
   )
